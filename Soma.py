d343b17165557b1db93087e70768713b480472ea
EnzoDias2006 <enzofontes68@gmail.com>
2024-09-03 13:23:27 -0300

    Add files via upload

---
+++ Soma.py
@@ -0,0 +1,12 @@
+def main():
+    INDICE = 12
+    SOMA = 0
+    K = 1
+
+    while K < INDICE:
+        K = K + 1
+        SOMA = SOMA + K
+    print(SOMA)
+
+if __name__ == "__main__":
+    main()
